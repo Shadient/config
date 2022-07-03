1940b982b86b9ea67e9e6be5f56056b3b7676a92
Shadient <85789237+Shadient@users.noreply.github.com>
2022-07-03 13:56:01 +0200

    Update and rename install-phase-I.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+# base devel
+sudo pacman -S base-devel
+
+# Languages
+  # python
+  sudo pacman -S python-pip # for Python3
+
+  # Node & NPM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  source ~/.bashrc
+  nvm install --lts
+  npm install -g npm@8.13.2
+  
+  # Rust
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source ~/.bashrc
+  cargo --version
+  
+# Packages
+sudo pacman -S qbittorrent 
