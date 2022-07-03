f40f6ff9dd713cf3d0221af0c3d7df9ec514459f
Shadient <85789237+Shadient@users.noreply.github.com>
2022-07-03 18:29:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ sudo pacman -S base-devel
 
   # Node & NPM
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  sleep 1
   source ~/.bashrc
+  sleep 1
   nvm install --lts
   npm install -g npm@8.13.2
   
