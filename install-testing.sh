d81beec10059b9702849dcb7d4a47aac72c1fc8f
Shadient <85789237+Shadient@users.noreply.github.com>
2022-07-03 22:21:09 +0200

    Update install-testing.sh

--- install-testing.sh
+++ install-testing.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-  
+ 
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac 
+ 
   # Node & NPM
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  source install-testing.sh
+  eval "$(cat ~/.bashrc | tail -n +10)"
   nvm install --lts
   npm install -g npm@8.13.2
