948b73f10d6bce5d432aeac96b14bebafbfff933
Shadient <85789237+Shadient@users.noreply.github.com>
2022-07-03 18:33:19 +0200

    Create install-testing.sh

---
+++ install-testing.sh
@@ -0,0 +1,7 @@
+  # Node & NPM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  sleep 1
+  source ~/.bashrc
+  sleep 1
+  nvm install --lts
+  npm install -g npm@8.13.2
