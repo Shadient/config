9925c6d2e7e41edc5409bd7b28916c3ea403a0f7
Shadient <85789237+Shadient@users.noreply.github.com>
2022-07-04 22:35:34 +0200

    Update install-polybar.sh

--- polybar/install-polybar.sh
+++ polybar/install-polybar.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+sudo pacman -S polybar
+
+mkdir -p .config/polybar
+wget -P .config/polybar https://raw.githubusercontent.com/Shadient/config/main/polybar/config.ini
+wget -P .config/polybar https://raw.githubusercontent.com/Shadient/config/main/polybar/launch.sh
+cd .config/polybar
+chmod +x launch.sh
